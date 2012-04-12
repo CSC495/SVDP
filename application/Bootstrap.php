5f3a799034df02f2a189bed9fd6abaaafa80d8b6
unknown <bagura@C-111-03.(none)>
2012-04-12 11:24:52 -0500

    Work in progress login controller.

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,15 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
-
+    Zend_Session::start();
+    
+    protected function _initConfig()
+    {
+        /*
+        Load configuration data
+        Zend_Registry::set('logo',$logo);
+        Zend_Registry::set('organization',$logo);
+        */
+    }
 }
 
--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -6,11 +6,12 @@ class IndexController extends Zend_Controller_Action
     public function init()
     {
         /* Initialize action controller here */
+        // Load site image into session
     }
 
     public function indexAction()
     {
-        // action body
+        $this->redirect('/login/index');
     }
 
 
