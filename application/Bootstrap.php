21a2e27a8db7357f921443f04402fb52a90acc69
Jonathan Rascher <jon@bcat.name>
2012-04-15 22:24:27 -0500

    Add Action Helper to verify authorization

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -7,5 +7,10 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
     {
         Zend_Session::start();
     }
+
+    public function _initActionHelpers()
+    {
+        Zend_Controller_Action_HelperBroker::addHelper(new App_Helper_AuthCheck());
+    }
 }
 
