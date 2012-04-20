719c963e4016a5b5939417f5fcc3a3b3a6349191
Jonathan Rascher <jon@bcat.name>
2012-04-20 16:32:39 -0500

    Remove deprecated AuthCheck action helper

--- application/controllers/SearchController.php
+++ application/controllers/SearchController.php
@@ -16,13 +16,11 @@ class SearchController extends Zend_Controller_Action
 
     public function memberAction()
     {
-        $this->_helper->authCheck(Application_Model_User::ROLE_MEMBER);
         $this->view->pageTitle = 'Client and Case Search';
     }
 
     public function treasurerAction()
     {
-        $this->_helper->authCheck(Application_Model_User::ROLE_TREASURER);
         $this->view->pageTitle = 'Check Request Search';
     }
 }
