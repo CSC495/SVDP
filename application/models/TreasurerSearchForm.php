8f319ad1f8b3611bbe5b5ff48232cd514b21b345
Jonathan Rascher <jon@bcat.name>
2012-04-21 17:20:48 -0500

    Add conditional validation for one-record searchs

--- application/models/TreasurerSearchForm.php
+++ application/models/TreasurerSearchForm.php
@@ -4,11 +4,28 @@ class Application_Model_TreasurerSearchForm extends Application_Model_SearchForm
 {
 
     private $additionalTypes = array(
-        'checkRequestId' => 'Check Request #',
+        Application_Model_SearchForm::TYPE_CLIENT_ID => array(
+            'label' => 'Client ID',
+        ),
+        Application_Model_SearchForm::TYPE_CASE_ID => array(
+            'label' => 'Case ID',
+        ),
+        Application_Model_SearchForm::TYPE_CHECK_REQ_ID => array(
+            'label' => 'Check Request ID',
+            'validators' => array(
+                array('Db_RecordExists', true, array(
+                    'table' => 'check_request',
+                    'field' => 'checkrequest_id',
+                    'messages' => array(
+                        'noRecordFound' => 'No check request was found for that ID.'
+                    ),
+                )),
+            ),
+        ),
     );
 
     public function __construct($options = null)
     {
-        parent::__construct('treasurer', $additionalTypes, $options);
+        parent::__construct('treasurer', $this->additionalTypes, $options);
     }
 }
