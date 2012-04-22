e099ce59b96a4413fa1c8cf77e8ab21931792a8b
Chris Heyman <ChrisHeyman@gmail.com>
2012-04-22 18:48:44 -0500

    Still working on Client/Case edit

---
+++ application/controllers/MemberController.php
@@ -0,0 +1,13 @@
+<?php
+
+class MemberController extends Zend_Controller_Action {
+	
+	public function init(){
+		$this->view->pagetitle = "Member Controller";
+	}
+	
+	public function indexAction() {
+		// TODO Auto-generated MemberController::indexAction() default action
+	}
+
+}
