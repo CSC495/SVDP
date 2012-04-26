df98384ba816dd687969dfa29e9e0dad7a0f9b8d
Thatoneguy123 <bagura@noctrl.edu>
2012-04-26 16:10:39 -0500

    Reworked login form interface

--- application/models/Login/LoginForm.php
+++ application/models/Login/LoginForm.php
@@ -10,15 +10,12 @@ class Application_Model_Login_LoginForm extends Zend_Form
 
         $baseUrl = new Zend_View_Helper_BaseUrl();
 		$this->setAction($baseUrl->baseUrl('/login/process'));
+		$this->setDecorators(array(
+			array('ViewScript', array('viewScript' => 'login/loginViewScript.phtml'))
+		));	
 		
-		// Username must consist of letters only
-		//          must be between 5 and 20 characters
+		// User Name
 		$username = $this->addElement('text', 'username', array(
-                                   'filters'    => array('StringTrim', 'StringToLower'),
-                                  'validators' => array(
-				          'Alnum',
-                                  array('StringLength', false, array(1, 20)),
-		                        ),
 				   'required'   => true,
 				   'label'      => 'Username:',
 				 ));
@@ -26,10 +23,6 @@ class Application_Model_Login_LoginForm extends Zend_Form
                // Password must consist of alphanumeric characters only
                //          must be between 6 and 20 characters
                $password = $this->addElement('password', 'password', array(
-                   'filters'    => array('StringTrim'),
-                   'validators' => array(
-                       array('StringLength', false, array(6, 20)),
-                   ),
                    'required'   => true,
                    'label'      => 'Password:',
                ));
@@ -38,13 +31,15 @@ class Application_Model_Login_LoginForm extends Zend_Form
                $login = $this->addElement('submit', 'login', array(
                    'required' => false,
                    'ignore'   => true,
-                   'label'    => 'Login',
+                   'label'    => '        Login        ',
+		   'class'    => 'btn-success',
                 ));
                
                 $forgot = $this->addElement('submit','forgot', array(
                     'required' => false,
                     'ignore' => true,
                     'label' => 'Forgot Password',
+		    'class' => 'btn-info',
                 ));
 	}
 }
