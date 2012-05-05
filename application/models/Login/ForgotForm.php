baa21ef0ffea4b07d745e6f78bea7882837d0062
Thatoneguy123 <bagura@noctrl.edu>
2012-05-04 22:06:35 -0500

    Cleanup on Login controller

--- application/models/Login/ForgotForm.php
+++ application/models/Login/ForgotForm.php
@@ -1,33 +1,55 @@
 <?php
+//class Application_Model_Login_ForgotForm extends Twitter_BootStrap_Form_Vertical
+//{
+//	public function __construct($options = null){
+//		
+//		parent::__construct($options);
+//		$this->setName('forgot');
+//		$this->setAttrib('id', 'forgot');
+//		$this->setMethod('post');
+//		$this->setDecorators(array('FormElements'));
+//
+//		$baseUrl = new Zend_View_Helper_BaseUrl();
+//		$this->setAction($baseUrl->baseUrl('/login/forgotprocess'));
+//		
+//		$this->addElement('text','username',array(
+//			'required'   => true,
+//			'filters'    => array('stringTrim'),
+//			'label'      => 'Username:',
+//		));
+//		
+//		$this->addDisplayGroup(array('username'), 'id', null);
+//
+//		$this->addElement('submit','submit', array(
+//			'buttonType'  => Twitter_Bootstrap_Form_Element_Submit::BUTTON_SUCCESS,
+//			'label'       => 'Send E-Mail',
+//			'decorators' => array('ViewHelper')
+//		));
+//	}
 class Application_Model_Login_ForgotForm extends Zend_Form
 {
-	
 	public function __construct($options = null){
 		parent::__construct($options);
 		$this->setName('forgot');
 		$this->setAttrib('id', 'forgot');
 		$this->setMethod('post');
 
-        $baseUrl = new Zend_View_Helper_BaseUrl();
-		$this->setAction($baseUrl->baseUrl('/login/forgot'));
+		$baseUrl = new Zend_View_Helper_BaseUrl();
+		$this->setAction($baseUrl->baseUrl('/login/forgotprocess'));
 		
-		// Username must consist of letters only
-		//          must be between 5 and 20 characters
-		$username = $this->addElement('text', 'username', array(
-                                   'filters'    => array('StringTrim', 'StringToLower'),
-                                  'validators' => array(
-				          'Alnum',
-                                  array('StringLength', false, array(1, 20)),
-		                        ),
-				   'required'   => true,
-				   'label'      => 'Username:',
-				 ));
 
-               $login = $this->addElement('submit', 'proceed', array(
+		$username = $this->addElement('text', 'username', array(
+			'required'   => true,
+		        'filters'   => array('stringTrim'),
+			'label'      => 'Username:',
+		));
+
+                $login = $this->addElement('submit', 'submit', array(
                    'required' => false,
                    'ignore'   => true,
-                   'label'    => 'E-mail Password',
+                   'label'    => 'E-Mail Password',
+		   'class'    => 'btn btn-success',
                 ));
-               
+
 	}
 }
