e030b71bfcc4287c49a7852571f07ce199279168
Jonathan Rascher <jon@bcat.name>
2012-05-05 14:36:47 -0500

    Get schedule editing almost working
    * Just need to wire up service layer for DB modification
    * Along the way, wrote some (hopefully) useful things for working with the
    various "record set" style forms
    * Also added a library file to allow passing serialized data to and from the
    client-side without tampering

---
+++ application/models/Member/ScheduleForm.php
@@ -0,0 +1,39 @@
+<?php
+
+class Application_Model_Member_ScheduleForm extends Zend_Form
+{
+
+    public function __construct($users)
+    {
+        parent::__construct();
+
+        $baseUrl = new Zend_View_Helper_BaseUrl();
+
+        $this
+            ->setAction($baseUrl->baseUrl(App_Resources::MEMBER) . '/editSchedule')
+            ->setMethod('post')
+            ->setDecorators(array(
+                'PrepareElements',
+                array('ViewScript', array(
+                    'viewScript' => 'form/schedule-form.phtml',
+                )),
+                array('form', array('class' => 'member form-horizontal')),
+            ));
+
+        $this->addSubForm(
+            new Application_Model_Member_ScheduleRecordListSubForm($users),
+            'scheduleRecordList'
+        );
+
+        $this->addElement('submit', 'submit', array(
+            'label' => 'Submit Changes',
+            'decorators' => array('ViewHelper'),
+            'class' => 'btn btn-success',
+        ));
+    }
+
+    public function preValidate($data)
+    {
+        $this->scheduleRecordList->preValidate($data);
+    }
+}
