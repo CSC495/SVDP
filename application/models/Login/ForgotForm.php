31b3dd013829b9290815414123fbd77933cc47c4
Thatoneguy123 <bagura@noctrl.edu>
2012-05-05 15:06:37 -0500

    Changed forms in login to post back to action which created them. Added validation to forgot form

--- application/models/Login/ForgotForm.php
+++ application/models/Login/ForgotForm.php
@@ -1,55 +1,39 @@
 <?php
-//class Application_Model_Login_ForgotForm extends Twitter_BootStrap_Form_Vertical
-//{
-//	public function __construct($options = null){
-//		
-//		parent::__construct($options);
-//		$this->setName('forgot');
-//		$this->setAttrib('id', 'forgot');
-//		$this->setMethod('post');
-//		$this->setDecorators(array('FormElements'));
-//
-//		$baseUrl = new Zend_View_Helper_BaseUrl();
-//		$this->setAction($baseUrl->baseUrl('/login/forgotprocess'));
-//		
-//		$this->addElement('text','username',array(
-//			'required'   => true,
-//			'filters'    => array('stringTrim'),
-//			'label'      => 'Username:',
-//		));
-//		
-//		$this->addDisplayGroup(array('username'), 'id', null);
-//
-//		$this->addElement('submit','submit', array(
-//			'buttonType'  => Twitter_Bootstrap_Form_Element_Submit::BUTTON_SUCCESS,
-//			'label'       => 'Send E-Mail',
-//			'decorators' => array('ViewHelper')
-//		));
-//	}
-class Application_Model_Login_ForgotForm extends Zend_Form
+class Application_Model_Login_ForgotForm extends Twitter_BootStrap_Form_Vertical
 {
 	public function __construct($options = null){
+		
 		parent::__construct($options);
+		
+		$this->addElementPrefixPath(
+			'Twitter_Bootstrap_Form_Decorator',
+			'Twitter/Bootstrap/Form/Decorator',
+			'decorator'
+		);
+		
 		$this->setName('forgot');
 		$this->setAttrib('id', 'forgot');
 		$this->setMethod('post');
 
 		$baseUrl = new Zend_View_Helper_BaseUrl();
-		$this->setAction($baseUrl->baseUrl('/login/forgotprocess'));
+		$this->setAction($baseUrl->baseUrl('/login/forgot'));
 		
-
-		$username = $this->addElement('text', 'username', array(
+		$this->addElement('text','username',array(
 			'required'   => true,
-		        'filters'   => array('stringTrim'),
+			'filters'    => array('stringTrim'),
 			'label'      => 'Username:',
-		));
-
-                $login = $this->addElement('submit', 'submit', array(
-                   'required' => false,
-                   'ignore'   => true,
-                   'label'    => 'E-Mail Password',
-		   'class'    => 'btn btn-success',
+			'validators' => array(
+				array('NotEmpty', true, array(
+			'type' => 'string',
+			'messages' => array('isEmpty' => 'Field is required.')))),
                 ));
+		
+		$this->addDisplayGroup(array('username'), 'id', null);
 
+		$this->addElement('submit','submit', array(
+			'buttonType'  => Twitter_Bootstrap_Form_Element_Submit::BUTTON_SUCCESS,
+			'label'       => 'Send E-Mail',
+			'decorators' => array('ViewHelper'),
+		));
 	}
 }
