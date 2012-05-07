442a5958a01da7202e7384eca892833198af2acf
Matthew Tieman <mathewti@aol.com>
2012-05-06 21:22:11 -0500

    Admin queries done

--- library/App/Service/AdminService.php
+++ library/App/Service/AdminService.php
@@ -49,36 +49,35 @@ class App_Service_AdminService {
         $this->_db->update('parish_funds',$data,'1');
     }
     
-    public function updateParishMember($user){
-	$userData = $this->disassembleParishParams($user);
-	$where = $this->_db->quoteInto('user_id = ?', $user->getId());
+    public function updateUserInfo($user){
+	$userData = $this->disassembleUserModel($user);
+	$where = $this->_db->quoteInto('user_id = ?', $user->getUserId());
 	$this->_db->update('user', $userData, $where);
     }
     
     public function updateUserPassword($userId, $newPass){
+	//Salting goes here when it is to be implemented
+	$hashPass =  hash('SHA256', $newPass);
 	$change = array(
-		    'password' => $newPass,
+		    'password' => $hashPass,
 		    'change_pswd' => '0');
 	$where = $this->_db->quoteInto('user_id = ?', $userId);
 	$this->_db->update('user', $change, $where);
     }
-    /***
-     *  Function gets an array of all the 
-     */
-    public function getParishMembers()
-    {
+
+    public function getAllUsers(){
         $select = $this->_db->select()
 		->from('user')
 		->order('first_name ASC');
 	$results = $this->_db->fetchAll($select);
-	return $this->buildMemeberList($results);
+	return $this->buildUserList($results);
     }
     
     /**
      *  Function builds an array of all memebers of the parish
      *  from a row set
      */
-    private function buildMemeberList($results){
+    private function buildUserList($results){
         $users = array();
         foreach($results as $row)
         {
@@ -99,7 +98,7 @@ class App_Service_AdminService {
     }
     
     public function buildUserModel($results){
-	$user = Application_Model_Impl_User();
+	$user = new Application_Model_Impl_User();
 	$user->setUserId($results['user_id']);
 	$user->setFirstName($results['first_name']);
 	$user->setLastName($results['last_name']);
@@ -111,6 +110,13 @@ class App_Service_AdminService {
 	return $user;
     }
     
+    public function createUser($user, $password){
+	$userData = $this->disassembleUserModel($user);
+	$hashPass =  hash('SHA256', $password);
+	$userData['password'] = $hashPass;
+	$userData['change_pswd'] = '1';
+	$this->_db->insert('user', $userData);
+    }
     /****
      *  Adds a user to the database
      */
@@ -129,23 +135,6 @@ class App_Service_AdminService {
                         );
         $result = $this->_db->insert('user',$params);
     }
-    
-    /***
-     *  Updates a users information
-     */
-    public function updateUserInformation($user)
-    {
-        $data = array(  
-                        'first_name'  => $user->getFirstName(),
-                        'last_name'   => $user->getLastName(),
-                        'email'       => $user->getEmail(),
-                        'cell_phone'  => $user->getCellPhone(),
-                        'home_phone'  => $user->getHomePhone(),
-                        'role'        => $user->getRole(),
-                        'active_flag' => $user->getActive());
-        
-        $this->_db->update('user',$data,"user_id ='" . $user->getUserId() . "'");
-    }
 
     /*****
      *  Resets a users password
@@ -156,17 +145,6 @@ class App_Service_AdminService {
                        'change_pswrd'  => 1);
         
         $this->_db->update('user',$data,"user_id ='" . $userId ."'");
-    }
-    
-    /***
-     *  Gets user information
-     */
-    public function getUserInfo($userId)
-    {
-        $select = $this->_db->select()->from('user')->where('user_id = ?',$userId);
-        
-        $results = $this->_db->fetchRow($select);
-        return $this->buildMemeber($results);   
     }
     
     /***
@@ -199,12 +177,14 @@ class App_Service_AdminService {
     
     public function disassembleUserModel($user){
 	$userData = array(
+		    'user_id' => $user->getUserId(),
 		    'first_name' => $user->getFirstName(),
 		    'last_name' => $user->getLastName(),
-		    'email' => $user->geEmail(),
+		    'email' => $user->getEmail(),
 		    'cell_phone' => $user->getCellPhone(),
 		    'home_phone' => $user->getHomePhone(),
 		    'role' => $user->getRole(),
 		    'active_flag' => $user->getActive());
+	return $userData;
     }
 }
