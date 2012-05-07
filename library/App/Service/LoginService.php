0804f0539a4794257f85a582a0422b7a1d058e42
Matthew Tieman <mathewti@aol.com>
2012-05-06 21:22:11 -0500

    Admin queries done

--- library/App/Service/LoginService.php
+++ library/App/Service/LoginService.php
@@ -6,29 +6,43 @@ class App_Service_LoginService {
     function __construct(){
         $this->_db = Zend_Db_Table::getDefaultAdapter();
     }
-       
-   
-    /****
-     *  Sets the users password
-     */
-    public function updateUserPassword($userId,$password)
-    { 
-        $data = array( 'password'      => $password,
-                       'change_pswd'  => 0);
 
-        $this->_db->update('user',$data, "user_id='" . $userId . "'");
-
+    public function updateUserPassword($userId, $password){ 
+        //Salting goes here when it is to be implemented
+	$hashPass =  hash('SHA256', $newPass);
+	$change = array(
+		    'password' => $hashPass,
+		    'change_pswd' => '0');
+	$where = $this->_db->quoteInto('user_id = ?', $userId);
+	$this->_db->update('user', $change, $where);
     }
     
-    /***
-     *  Gets user information
-     */
-    public function getUserInfo($userId)
-    {
-        $select = $this->_db->select()->from('user')->where('user_id = ?',$userId);
-        
+    public function updateDocument($id, $doc){
+        $docData = $this->disassembleDocument($doc);
+        $docData['doc_id'] = $id;
+        $where = $this->_db->quoteInto('doc_id = ?', $id);
+        $this->_db->update('documents', $docData, $where);
+    }
+    
+    public function getUserInfo($userId){
+        $select = $this->_db->select()
+                ->from('user')
+                ->where('user_id = ?', $userId);
         $results = $this->_db->fetchRow($select);
-        return $this->buildMemeber($results);   
+        return $this->buildMemeber($results);
+    }
+    
+    public function getDocumentById($id){
+        $select = $this->_db->select()
+                ->from('documents')
+                ->where('doc_id = ?', $id);
+        $results = $this->_db->fetchRow($select);
+        return $this->buildDocument($results);
+    }
+    
+    public function deleteDocument($id){
+        $where = $this->_db->quoteInto('doc_id = ?', $id);
+        $this->_db->delete('documents', $where);
     }
     
     /***
@@ -45,9 +59,26 @@ class App_Service_LoginService {
             ->setCellPhone($row['cell_phone'])
             ->setHomePhone($row['home_phone'])
             ->setRole($row['role'])
+            ->setChangePswdFlag($row['change_pswd'])
             ->setActive($row['active_flag']);
-        
         return($user);
     }
     
+    private function buildDocument($row){
+        $doc = new Application_Model_Impl_Document();
+        $doc
+            ->setId($row['doc_id'])
+            ->setUrl($row['url'])
+            ->setName($row['filename'])
+            ->setInternal($row['internal_flag']);
+        return $doc;
+    }
+    
+    private function disassembleDocument($doc){
+        $docData = array(
+                         'filename' => $doc->getName(),
+                         'url' => $doc->getUrl(),
+                         'internal_flag' => $doc->isInternal());
+        return $docData;
+    }
 }
