cccf4cbbf0b1e4891d75778791286a3c559fb4a1
gwdickson <gwdickson@noctrl.edu>
2012-05-08 11:49:01 -0500

    updated reports with formatting changes

--- application/models/Report/reportForm.php
+++ application/models/Report/reportForm.php
@@ -1,46 +1,37 @@
 <?php
-class Application_Model_Report_reportForm extends Zend_Form
+class Application_Model_Report_reportForm extends Twitter_BootStrap_Form_Vertical
 {
 	
 	public function __construct($options = null){
 		parent::__construct($options);
-		$this->setName('login');
-		$this->setAttrib('id', 'login');
+		$this->setName('report');
+		$this->setAttrib('id', 'report');
 		$this->setMethod('post');
-		//$this->setAction('/SVDP/public/report/process');  
 		
 		$baseUrl = new Zend_View_Helper_BaseUrl();
 		$this->setAction($baseUrl->baseUrl('/report/process'));
 		
-		$report1Label = $this->addElement('text', 'report1Label', array(
-                   'required' => false,
-                   'ignore'   => true,
-                   'label'    => 'Reimbursement Form',
-                ));
+		$this->setDecorators(array(
+			array('ViewScript', array('viewScript' => 'report/indexFormViewScript.phtml'))
+		));		
+		
                 $report1 = $this->addElement('submit', 'report1', array(
                    'required' => false,
                    'ignore'   => true,
                    'label'    => 'Generate Report',
-                ));
-		$report2Label = $this->addElement('text', 'report2Label', array(
-                   'required' => false,
-                   'ignore'   => true,
-                   'label'    => 'On Call Activities',
+		   'class'    => 'btn btn-success',
                 ));
                 $report2 = $this->addElement('submit', 'report2', array(
                    'required' => false,
                    'ignore'   => true,
                    'label'    => 'Generate Report',
-                ));
-		$report3Label = $this->addElement('text', 'report3Label', array(
-                   'required' => false,
-                   'ignore'   => true,
-                   'label'    => 'Client Information',
+		   'class'    => 'btn btn-success',
                 ));
                 $report3 = $this->addElement('submit', 'report3', array(
                    'required' => false,
                    'ignore'   => true,
                    'label'    => 'Generate Report',
+		   'class'    => 'btn btn-success',
                 )); 
 	}
 }
