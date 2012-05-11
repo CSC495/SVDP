924ebd3346dc7b4b42e830357018c2e618a4d327
Thatoneguy123 <bagura@noctrl.edu>
2012-05-11 16:34:48 -0500

    Fixed mismatched column names

--- library/App/Service/DocumentService.php
+++ library/App/Service/DocumentService.php
@@ -56,7 +56,7 @@ class App_Service_DocumentService {
                 ->setId($row['doc_id'])
                 ->setUrl($row['url'])
                 ->setName($row['filename'])
-                ->setInternal($row['internal']);
+                ->setInternal($row['internal_flag']);
                 
         return($doc);
     }
@@ -75,7 +75,7 @@ class App_Service_DocumentService {
         $data = array(  
                         'filename'    => $doc->getName(),
                         'url'         => $doc->getUrl(),
-                        'internal'    => $doc->isInternal());
+                        'internal_flag'    => $doc->isInternal());
         $where = "doc_id = " . $doc->getId();
         
         $this->_db->update('documents',$data,$where);
@@ -89,8 +89,7 @@ class App_Service_DocumentService {
         $data = array(  'doc_id'      => null,
                         'filename'    => $doc->getName(),
                         'url'         => $doc->getUrl(),
-                        'internal'    => $doc->isInternal());
-        
+                        'internal_flag'    => $doc->isInternal());
         $this->_db->insert('documents',$data);
     }
 }
