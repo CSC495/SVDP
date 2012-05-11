2d695008a09a24423bce69e03e74e3adc7c6d5e6
Kurt Siegfried <kurt.siegfried@gmail.com>
2012-05-11 16:17:12 -0500

    Modify bootstrap to load AWS env vars from system, and set them to Zend_Registry.

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -27,5 +27,11 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $params = $service->getParishParams();
         Zend_Registry::set('config', $params);
     }
+
+	protected function _initAWSParams()
+	{
+		Zend_Registry::set('AWS_ACCESS_KEY_ID',getenv('AWS_ACCESS_KEY_ID'));
+		Zend_Registry::set('AWS_SECRET_ACCESS_KEY',getenv('AWS_SECRET_ACCESS_KEY'));
+	}
 }
 
