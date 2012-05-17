4694624bec8637253c71cedd545379ce76bbb8c2
unknown <gwdickson@C-111-05.(none)>
2012-05-17 11:17:26 -0500

    updated reports

--- application/models/Report/clientReport.php
+++ application/models/Report/clientReport.php
@@ -1,54 +1,33 @@
 <?php
-class Application_Model_Report_clientReport extends Zend_Form
+class Application_Model_Report_clientReport extends Twitter_Bootstrap_Form_Vertical
 {
 	
 	public function __construct($options = null){
 		parent::__construct($options);
-		$this->setName('login');
-		$this->setAttrib('id', 'login');
+		$this->setName('clientinfo');
+		$this->setAttrib('id', 'clientinfo');
 		$this->setMethod('post');
-		$this->setAction('');
 		
-		// Username must consist of letters only
-		//          must be between 1 and 20 characters
-		$firstName = $this->addElement('text', 'firstName', array(
-                                   'filters'    => array('StringTrim', 'StringToLower'),
-                                  'validators' => array(
-				          'Alnum',
-                                  array('StringLength', false, array(1, 20)),
-		                        ),
-				   'required'   => true,
-				   'label'      => 'First Name:',
-				 ));
+		$baseUrl = new Zend_View_Helper_BaseUrl();
+		$this->setAction($baseUrl->baseUrl('/report/clientresults/'));
 		
-               // lastname must consist of alphanumeric characters only
-               //          must be between 1 and 20 characters
-               $lastName = $this->addElement('text', 'lastName', array(
-                   'filters'    => array('StringTrim'),
-                   'validators' => array(
-                       'Alnum',
-                       array('StringLength', false, array(1, 20)),
-                   ),
-                   'required'   => true,
-                   'label'      => 'Last Name:',
-               ));
-               // lastname must consist of alphanumeric characters only
-               //          must be between 1 and 13 characters
-               $phoneNum = $this->addElement('text', 'phoneNum', array(
-                   'filters'    => array('StringTrim'),
-                   'validators' => array(
-                       'Alnum',
-                       array('StringLength', false, array(6, 20)),
-                   ),
-                   'required'   => true,
-                   'label'      => 'Phone Number:',
-               ));
-        
+		$this->setDecorators(array(
+			array('ViewScript', array('viewScript' => 'report/clientinfoViewScript.phtml'))
+		));	
+		
+		// Client Id(Integer)
+		$clientID = $this->addElement('text', 'clientid', array(
+			'filters'    => array('Digits'),
+			'required'   => true,
+			'class'	     => 'input-small',
+			));
 		
                $login = $this->addElement('submit', 'create', array(
                    'required' => false,
                    'ignore'   => true,
                    'label'    => 'Create Report',
+		   'class'    => 'btn btn-success',
+		   'decorators' => array('ViewHelper'),
                ));           
 	}
 }
--- application/models/Report/reimbursementReport.php
+++ application/models/Report/reimbursementReport.php
@@ -1,5 +1,5 @@
 <?php
-class Application_Model_Report_clientReport extends Zend_Form
+class Application_Model_Report_reimbursementReport extends Twitter_Bootstrap_Form_Vertical
 {
 	
 	public function __construct($options = null){
@@ -9,46 +9,60 @@ class Application_Model_Report_clientReport extends Zend_Form
 		$this->setMethod('post');
 		$this->setAction('');
 		
-		// Username must consist of letters only
-		//          must be between 1 and 20 characters
-		$firstName = $this->addElement('text', 'firstName', array(
-                                   'filters'    => array('StringTrim', 'StringToLower'),
-                                  'validators' => array(
-				          'Alnum',
-                                  array('StringLength', false, array(1, 20)),
-		                        ),
-				   'required'   => true,
-				   'label'      => 'First Name:',
-				 ));
+		$baseUrl = new Zend_View_Helper_BaseUrl();
+		$this->setAction($baseUrl->baseUrl('/report/reimbursementreport/'));
 		
-               // lastname must consist of alphanumeric characters only
-               //          must be between 1 and 20 characters
-               $lastName = $this->addElement('text', 'lastName', array(
-                   'filters'    => array('StringTrim'),
-                   'validators' => array(
-                       'Alnum',
-                       array('StringLength', false, array(1, 20)),
-                   ),
-                   'required'   => true,
-                   'label'      => 'Last Name:',
-               ));
-               // lastname must consist of alphanumeric characters only
-               //          must be between 1 and 13 characters
-               $phoneNum = $this->addElement('text', 'phoneNum', array(
-                   'filters'    => array('StringTrim'),
-                   'validators' => array(
-                       'Alnum',
-                       array('StringLength', false, array(6, 20)),
-                   ),
-                   'required'   => true,
-                   'label'      => 'Phone Number:',
-               ));
-        
+		$this->setDecorators(array(
+			array('ViewScript', array('viewScript' => 'report/reimbursementViewScript.phtml'))
+		));
+		
+		// Start Date
+		$startDate = $this->addElement('text', 'startDate', array(
+                    'required' => true,
+                    'filters' => array('StringTrim'),
+                    'validators' => array(
+                        array('NotEmpty', true, array(
+                            'type' => 'string',
+                            'messages' => array('isEmpty' => 'Must choose a start date.'),
+                        )),
+                        array('Date', true, array(
+                            'format' => 'MM/dd/yyyy',
+                            'messages' => array(
+                                'dateInvalidDate' => 'Must be properly formatted.',
+                                'dateFalseFormat' => 'Must be a valid date.',
+                            ),
+                        )),
+                    ),
+                    'maxlength' => 10,
+                    'class' => 'span2 date',
+                ));
+                // End Date
+		$endDate = $this->addElement('text', 'endDate', array(
+                    'required' => true,
+                    'filters' => array('StringTrim'),
+                    'validators' => array(
+                        array('NotEmpty', true, array(
+                            'type' => 'string',
+                            'messages' => array('isEmpty' => 'Must choose a end date.'),
+                        )),
+                        array('Date', true, array(
+                            'format' => 'MM/dd/yyyy',
+                            'messages' => array(
+                                'dateInvalidDate' => 'Must be properly formatted.',
+                                'dateFalseFormat' => 'Must be a valid date.',
+                            ),
+                        )),
+                    ),
+                    'maxlength' => 10,
+                    'class' => 'span2 date',
+                ));
 		
                $login = $this->addElement('submit', 'create', array(
                    'required' => false,
                    'ignore'   => true,
                    'label'    => 'Create Report',
+		   'class'    => 'btn btn-success',
+		   'decorators' => array('ViewHelper'),
                ));           
 	}
 }
