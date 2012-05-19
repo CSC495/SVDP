61255ca7335198d41c14f30616ece5f6b7631b62
Jonathan Rascher <jon@bcat.name>
2012-05-19 11:36:58 -0500

    Make navigation code cleaner and more MVC friendly

--- library/App/Controller/Plugin/NavPlugin.php
+++ library/App/Controller/Plugin/NavPlugin.php
@@ -8,11 +8,31 @@ class App_Controller_Plugin_NavPlugin extends Zend_Controller_Plugin_Abstract
 
     public function postDispatch(Zend_Controller_Request_Abstract $request)
     {
-        $view = Zend_Layout::getMvcInstance()->getView();
+        // Get navigation links for the current user role.
+        $auth     = Zend_Auth::getInstance();
+        $role     = $auth->hasIdentity() ? $auth->getIdentity()->role : App_Roles::GENERAL;
+        $navLinks = App_Nav::getNavLinksByRole($role);
 
-        $view->nav = array(
-            'controller' => $request->getControllerName(),
-            'action' => $request->getActionName(),
-        );
+        // Determine current path for active tab highlighting.
+        $controller = $request->getControllerName();
+        $action     = $request->getActionName();
+
+        $currentPath = "/$controller";
+
+        if ($action !== 'index') {
+            $currentPath .= "/$action";
+        }
+
+        // Mark active navigation links.
+        foreach ($navLinks as $key => &$val) {
+            $val = array(
+                'text' => $val,
+                'active' => $key === $currentPath,
+            );
+        }
+
+        // Propagate navigation links to the view.
+        $view           = Zend_Layout::getMvcInstance()->getView();
+        $view->navLinks = $navLinks;
     }
 }
