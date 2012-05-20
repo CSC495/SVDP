967e0b8ce27e23eccbe35fa84f5eedc2c5ab9d28
Jonathan Rascher <jon@bcat.name>
2012-05-20 02:29:52 -0500

    Point member check req view links to treasurer

--- application/models/Member/CaseNeedRecordListSubForm.php
+++ application/models/Member/CaseNeedRecordListSubForm.php
@@ -203,7 +203,7 @@ class Application_Model_Member_CaseNeedRecordListSubForm
             $this->setSubFormReadOnly($caseNeedSubForm, true);
 
             $checkReq        = $referralOrCheckReq;
-            $viewCheckReqUrl = $baseUrl->baseUrl(App_Resources::MEMBER . '/viewCheckReq/id/'
+            $viewCheckReqUrl = $baseUrl->baseUrl(App_Resources::TREASURER . '/checkReq/id/'
                 . urlencode($checkReq->getId()));
 
             $status2 = '<a href="' . htmlspecialchars($viewCheckReqUrl) . '">';
