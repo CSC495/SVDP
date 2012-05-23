a54d36acdbc76b5edab46d87940786751633ce04
Thatoneguy123 <bagura@noctrl.edu>
2012-05-23 16:11:55 -0500

    Clean up js file.

--- public/document.js
+++ public/document.js
@@ -1,11 +1,5 @@
 var elements = document.getElementsByClassName("btn btn-mini btn-danger");
 
 for(var i=0; i<elements.length; i++) {
-    //elements[i].onclick = showConfirm;
 	elements[i].onclick=function(){return confirm("Are you sure you wish to delete document?\nCannot be recovered!")};
-}
-
-function showConfirm()
-{
-	return confirm('Are you sure you wish to delete document? Cannot be recovered!');
 }
