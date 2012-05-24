615516662e9acd067b06c852319255b1049d7f58
Thatoneguy123 <bagura@noctrl.edu>
2012-05-24 13:17:41 -0500

    Added PHPDoc markup

--- application/models/Impl/ParishParams.php
+++ application/models/Impl/ParishParams.php
@@ -4,7 +4,7 @@
  */
 class Application_Model_Impl_ParishParams
 {
-	/**
+    /**
      * The current funds available for the parish
      * @var float
      */
@@ -30,15 +30,14 @@ class Application_Model_Impl_ParishParams
      */
     private $_caseFundLimit;
     
-	/**
+    /**
      * Constructor for building a paraish params object
      *
      * @param float $funds Total funds available for the parish
      * @param int $year Number of cases a client may have in a year
-	 * @param float $lifetime Dollar amount of aid a client may recieve in their lifetime
-	 * @param int $caseLimit Number of cases a client may have in their lifetime
-	 * @param float $caseFundLimit Dollar amount limit a particular case may have
-     * @return void
+     * @param float $lifetime Dollar amount of aid a client may recieve in their lifetime
+     * @param int $caseLimit Number of cases a client may have in their lifetime
+     * @param float $caseFundLimit Dollar amount limit a particular case may have
      */
     public function __construct($funds,$year,$lifetime,$caseLimit,$caseFundLimit)
     {
@@ -49,98 +48,93 @@ class Application_Model_Impl_ParishParams
         $this->_caseFundLimit = $caseFundLimit;
     }
 
-	/**
-	 * Gets the amount of funds currently available
-	 *
-	 * @return float Total amount of funds available
-	 */
+    /**
+     * Gets the amount of funds currently available
+     *
+     * @return float Total amount of funds available
+     */
     public function getAvailableFunds(){
         return $this->_fundsAvailable;
     }
     /**
-	 * Sets the amount of funds available
-	 *
-	 * @param float $amt Amount of funds currently available
-	 *
-	 * @return Application_Model_Impl_ParishParams this
-	 */
+     * Sets the amount of funds available
+     *
+     * @param float $amt Amount of funds currently available
+     * @return Application_Model_Impl_ParishParams this
+     */
     public function setAvailableFunds($amt){
         $this->_fundsAvailable = $amt;
         return $this;
     }
     /**
-	 * Gets the number of cases a client may have in a year
-	 *
-	 * @retun int Number of cases a client may have per year
-	 */
+     * Gets the number of cases a client may have in a year
+     *
+     * @retun int Number of cases a client may have per year
+     */
     public function getYearlyLimit(){
         return $this->_yearLimit;
     }
     /**
-	 * Sets the number of cases a client may have per year
-	 *
-	 * @param int $amt Number of cases a client may have per year
-	 *
-	 * @return Application_Model_Impl_ParishParams this
-	 */
+     * Sets the number of cases a client may have per year
+     *
+     * @param int $amt Number of cases a client may have per year
+     * @return Application_Model_Impl_ParishParams this
+     */
     public function setYearlyLimit($amt){
         $this->_yearLimit = $amt;
         return $this;
     }
     /**
-	 * Sets the dollar amount of aid a client may recieve in their lifetime
-	 *
-	 * @param float $amt Amount of funds a client may recieve in their lifetime
-	 *
-	 * @return Application_Model_Impl_ParishParams this
-	 */
+     * Sets the dollar amount of aid a client may recieve in their lifetime
+     *
+     * @param float $amt Amount of funds a client may recieve in their lifetime
+     * @return Application_Model_Impl_ParishParams this
+     */
     public function setLifeTimeLimit($amt){
         $this->_lifeTimeLimit = $amt;
         return $this;
     }
     /**
-	 * Gets the dollar amount of aid a client may recieve in their lifetime
-	 *
-	 * @return float Dollar amount of aid a client may recieve in their lifetime
-	 */
+     * Gets the dollar amount of aid a client may recieve in their lifetime
+     *
+     * @return float Dollar amount of aid a client may recieve in their lifetime
+     */
     public function getLifeTimeLimit(){
         return $this->_lifeTimeLimit;
     }
     /**
-	 * Sets the number of cases a client may have in their lifetime
-	 *
-	 * @param int $amt Number of cases a client may have in their lifetime
-	 *
-	 * @return Application_Model_Impl_ParishParams this
-	 */
+    * Sets the number of cases a client may have in their lifetime
+    *
+    * @param int $amt Number of cases a client may have in their lifetime
+    * @return Application_Model_Impl_ParishParams this
+    */
     public function setCaseLimit($amt){
         $this->_caseLimit = $amt;
         return $this;
     }
-	/**
-	 * Gets the number of cases a client may have in their lifetime
-	 *
-	 * @return int number of cases a client may have in their lifetime
-	 */
+    /**
+     * Gets the number of cases a client may have in their lifetime
+     *
+     * @return int number of cases a client may have in their lifetime
+     */
     public function getCaseLimit(){
         return $this->_caseLimit;
     }
     /**
-	 * Sets the dollar amount limit per case
-	 *
-	 * @param int $amt The dollar amount limit per case
-	 *
-	 * @return Application_Model_Impl_ParishParams this
-	 */
+    * Sets the dollar amount limit per case
+    *
+    * @param int $amt The dollar amount limit per case
+    * @return Application_Model_Impl_ParishParams this
+    */
     public function setCaseFundLimit($amt){
         $this->_caseFundLimit = $amt;
         return $this;
     }
     /**
-	 * Gets the dollar amount limit per case
-	 *
-	 * @return int The dollar amount limit per case
-	 */
+    * Gets the dollar amount limit per case
+    *
+    * @return int The dollar amount limit per case
+    */
     public function getCaseFundLimit(){
         return $this->_caseFundLimit;
     }
--- application/models/Impl/User.php
+++ application/models/Impl/User.php
@@ -4,7 +4,7 @@
  */
 class Application_Model_Impl_User
 {
-	/**
+    /**
      * The users unique ID
      * @var int
      */
@@ -50,194 +50,194 @@ class Application_Model_Impl_User
      */
     private $_changePswdFlag;
     /**
-	 * Function sets the users ID
-	 * 
-	 * @param string $userId the users id
-	 *
-	 * @return Application_Model_Impl_User this
-	 */
+    * Function sets the users ID
+    * 
+    * @param string $userId the users id
+    *
+    * @return Application_Model_Impl_User this
+    */
     public function setUserId($userId){
         $this->_userId = $userId;
         return $this;
     }
     /**
-	 * Function gets the users ID
-	 *
-	 * @return string The users id
-	 */
+    * Function gets the users ID
+    *
+    * @return string The users id
+    */
     public function getUserId(){
         return $this->_userId;
     }
     /**
-	 * Function sets the users emaill address
-	 *
-	 * @param string $email the users email
-	 * @return Application_Model_Impl_User this
-	 */
+    * Function sets the users emaill address
+    *
+    * @param string $email the users email
+    * @return Application_Model_Impl_User this
+    */
     public function setEmail($email){
         $this->_email = $email;
         return $this;
     }
     /**
-	 * Function gets the users email address
-	 *
-	 * @return string The users email
-	 */
+    * Function gets the users email address
+    *
+    * @return string The users email
+    */
     public function getEmail(){
         return $this->_email;
     }
     /**
-	 * Function sets the users first name
-	 *
-	 * @param string $fn The users first name
-	 *
-	 * @return Application_Model_Impl_User this
-	 */
+    * Function sets the users first name
+    *
+    * @param string $fn The users first name
+    *
+    * @return Application_Model_Impl_User this
+    */
     public function setFirstName($fn){
         $this->_firstName = $fn;
         return $this;
     }
     /**
-	 * Gets the users first name
-	 *
-	 * @return string Users first name
-	 */
+    * Gets the users first name
+    *
+    * @return string Users first name
+    */
     public function getFirstName(){
         return $this->_firstName;
     }
     /**
-	 * Sets the users last name
-	 *
-	 * @param string $ln User's last name
-	 *
-	 * @return Application_Model_Impl_User this
-	 */
+    * Sets the users last name
+    *
+    * @param string $ln User's last name
+    *
+    * @return Application_Model_Impl_User this
+    */
     public function setLastName($ln){
         $this->_lastName = $ln;
         return $this;
     }
     /**
-	 * Returns the users last name
-	 *
-	 * @return string The users last name
-	 */
+    * Returns the users last name
+    *
+    * @return string The users last name
+    */
     public function getLastName(){
         return $this->_lastName;
     }
-	/**
-	 * Sets the users cell phone number
-	 *
-	 * @param string $phone The users cell phone number
-	 *
-	 * @return Application_Model_Impl_User this
-	 */
+    /**
+     * Sets the users cell phone number
+     *
+     * @param string $phone The users cell phone number
+     *
+     * @return Application_Model_Impl_User this
+     */
     public function setCellPhone($phone){
         $this->_cellPhone = $phone;
         return $this;
     }  
-	/**
-	 * Gets the users cell phone number
-	 *
-	 * @return string Users cell phone number
-	 */
+    /**
+     * Gets the users cell phone number
+     *
+     * @return string Users cell phone number
+     */
     public function getCellPhone(){
         return $this->_cellPhone;
     }
-	/**
-	 * Sets the users home phone number
-	 *
-	 * @param string $phone The users home phone number
-	 *
-	 * @return Application_Model_Impl_User this
-	 */
+    /**
+     * Sets the users home phone number
+     *
+     * @param string $phone The users home phone number
+     *
+     * @return Application_Model_Impl_User this
+     */
     public function setHomePhone($phone){
         $this->_homePhone = $phone;
         return $this;
     }
-	/**
-	 * Gets the users home phone number
-	 *
-	 * @return string Users home phone number
-	 */
+    /**
+     * Gets the users home phone number
+     *
+     * @return string Users home phone number
+     */
     public function getHomePhone(){
         return $this->_homePhone;
     }
-	/**
-	 * Sets the users role
-	 *
-	 * @param string $role The users role
-	 *
-	 * @return Application_Model_Impl_User this
-	 */
+    /**
+     * Sets the users role
+     *
+     * @param string $role The users role
+     *
+     * @return Application_Model_Impl_User this
+     */
     public function setRole($role){
         $this->_role = $role;
         return $this;
     }
-	/**
-	 * Gets the users role
-	 *
-	 * @return string The users role
-	 */
+    /**
+     * Gets the users role
+     *
+     * @return string The users role
+     */
     public function getRole(){
         return $this->_role;
     }
-	/**
-	 * Sets the Active flag
-	 *
-	 * @param int $val The active flag
-	 *
-	 * @return Application_Model_Impl_User this
-	 */
+    /**
+     * Sets the Active flag
+     *
+     * @param int $val The active flag
+     *
+     * @return Application_Model_Impl_User this
+     */
     public function setActive($val){
         $this->_active = $val;
         return $this;
     }
-	/**
-	 * Gets the change password flag
-	 *
-	 * @return int $flag change password flag
-	 */
+    /**
+     * Gets the change password flag
+     *
+     * @return int $flag change password flag
+     */
     public function getChangePswdFlag(){
         return $this->_changePswdFlag;
     }
-	/**
-	 * Sets the change password flag
-	 *
-	 * @param int $flag change password flag
-	 *
-	 * @return Application_Model_Impl_User this
-	 */
+    /**
+     * Sets the change password flag
+     *
+     * @param int $flag change password flag
+     *
+     * @return Application_Model_Impl_User this
+     */
     public function setChangePswdFlag($flag){
         $this->_changePswdFlag = $flag;
         return $this;
     }
-	/**
-	 * Gets the Active flag as boolean value
-	 *
-	 * @return bool True if user active false otherwise
-	 */
+    /**
+     * Gets the Active flag as boolean value
+     *
+     * @return bool True if user active false otherwise
+     */
     public function isActive(){
         if($this->_active == 1)
             return true;
         else
             return false;
     }
-	/**
-	 * Gets the Active flag as int value
-	 *
-	 * @return int 1 if user active 0 otherwise
-	 */
+    /**
+     * Gets the Active flag as int value
+     *
+     * @return int 1 if user active 0 otherwise
+     */
     public function getActive(){
         if(!$this->_active)
             return 0;
-		else
-			return 1;
+	else
+	    return 1;
     }
-	/**
-	 * Gets the users full name
-	 *
-	 * @return string First and last name appeneded together
-	 */
+    /**
+     * Gets the users full name
+     *
+     * @return string First and last name appeneded together
+     */
     public function getFullName()
     {
         if ($this->_firstName === null) {
