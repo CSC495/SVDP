c614aec9ef3ca16da36e86f63a96e8522833281a
Thatoneguy123 <bagura@noctrl.edu>
2012-05-24 14:55:34 -0500

    Added PhpDoc markup

--- public/document.js
+++ public/document.js
@@ -1,4 +1,4 @@
-var elements = document.getElementsByClassName("btn btn-mini btn-danger");
+var elements = document.getElementsByClassName('btn btn-mini btn-danger');
 
 for(var i=0; i<elements.length; i++) {
 	elements[i].onclick=function(){return confirm("Are you sure you wish to delete document?\nCannot be recovered!")};
