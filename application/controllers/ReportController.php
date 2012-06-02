cd4462c2362583bd00f23fb583b6579f3b1a4424
gwdickson <gwdickson@noctrl.edu>
2012-06-01 23:20:54 -0500

    reports

--- application/controllers/ReportController.php
+++ application/controllers/ReportController.php
@@ -56,14 +56,81 @@ class ReportController extends Zend_Controller_Action
 	$form->populate($_POST);
 	$cId = $form->clientid->getValue();
 	$service = new App_Service_Member();	
-	$this->view->client = $service->getClientById($cId);
-	
+	$this->view->client = $service->getClientById($cId);	
+    }
+    public function clientpdfAction(){
+	$this->view->pageTitle = "Client Information Report";
+	$form = new Application_Model_Report_clientReport();
+	$form->populate($_POST);
+	$cId = $form->clientid->getValue();
+	$service = new App_Service_Member();	
+	$this->view->client = $service->getClientById($cId);	
     }
     public function ocactivitiesAction(){
 	$this->view->pageTitle = "On Call Activities Report";
 	$this->view->form = new Application_Model_Report_ocaReport();
     }
     public function ocactivitiesresultsAction(){
+	$this->view->pageTitle = "On Call Activities Report";
+	$form = new Application_Model_Report_ocaReport();
+	$form->populate($_POST);
+	$start = $form->startDate->getValue();
+	$end = $form->endDate->getValue();
+	$this->view->start = $start;
+	$this->view->end = $end;
+	$service = new App_Service_DocumentService();
+	//calculate total miles for cases
+	$homeVisit = 0;
+	$teleVisit = 0;
+	$totalMiles = 0;
+	$teleHelped = 0;
+	$homeHelped = 0;
+	$miles = $service->getCaseVisitMiles($start, $end);
+	foreach($miles as $key=>$value)
+	{
+	    //if miles is greater than zero its a home visit
+	    if($value->getTotalMiles() > 0)
+	    {
+		$homeVisit += 1;
+		$totalMiles += $value->getTotalMiles();
+		$homeHelped += $value->getNumHMembers();
+	    }
+	    else  //else its a phone visit
+	    {
+		$teleVisit += 1;
+		$teleHelped += $value->getNumHMembers();
+	    }
+	    
+	}
+	$this->view->home = $homeVisit;
+	$this->view->tele = $teleVisit;
+	$this->view->totalMiles = $totalMiles;
+	$this->view->homeHelped = $homeHelped;
+	$this->view->teleHelped = $teleHelped;
+	
+	$totalHours = 0;
+	$hours = $service->getCaseVisitHours($start, $end);
+	foreach($hours as $row)
+	{
+	    $totalHours = $totalHours + $row;	    
+	}
+	$this->view->totalHours = $totalHours;
+	
+	$refer = $service->getGenReports($start, $end);
+	$referrals = 0;
+	$referHelped = 0;
+	
+	foreach($refer as $row)
+	{
+	    $referrals += $row->getNumRefs();
+	    $referHelped += $row->getNumHMembers();
+	}
+	
+	$this->view->referrals = $referrals;
+	$this->view->referHelped = $referHelped;
+	
+    }
+    public function ocapdfAction(){
 	$this->view->pageTitle = "On Call Activities Report";
 	$form = new Application_Model_Report_ocaReport();
 	$form->populate($_POST);
