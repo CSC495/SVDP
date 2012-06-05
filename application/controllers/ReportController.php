00853e17235ced9763593e818454447ef345df93
gwdickson <gwdickson@noctrl.edu>
2012-06-05 09:57:57 -0500

    reports

--- application/controllers/ReportController.php
+++ application/controllers/ReportController.php
@@ -103,6 +103,10 @@ class ReportController extends Zend_Controller_Action
 	$totalMiles = 0;
 	$teleHelped = 0;
 	$homeHelped = 0;
+	if($end == null || $start == null)
+	{
+	    $this->_helper->redirector('error');
+	}
 	$miles = $service->getCaseVisitMiles($start, $end);
 	foreach($miles as $key=>$value)
 	{
@@ -145,7 +149,8 @@ class ReportController extends Zend_Controller_Action
 	}
 	
 	$this->view->referrals = $referrals;
-	$this->view->referHelped = $referHelped;	
+	$this->view->referHelped = $referHelped;
+	
     }
     /**queries database on call activities information displays in pdf*/ 
     public function ocapdfAction(){
